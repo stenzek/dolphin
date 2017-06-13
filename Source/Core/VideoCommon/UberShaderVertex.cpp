4ec199d8675440960aa8a8fa8777cc379263a766
Stenzek <stenzek@gmail.com>
2017-06-13 23:23:05 +1000

    UberShaderVertex: Emit same vertex setup statements as normal shaders

--- Source/Core/VideoCommon/UberShaderVertex.cpp
+++ Source/Core/VideoCommon/UberShaderVertex.cpp
@@ -168,54 +168,94 @@ ShaderCode GenVertexShader(APIType ApiType, const vertex_ubershader_uid_data* ui
 		out.Write("		o.tex[%d].xyz = float3(dot(coord, " I_TEXMATRICES"[%d]), dot(coord, " I_TEXMATRICES"[%d]), 1);\n", i, 3 * i, 3 * i + 1);
 		out.Write("	}\n");
 	}
-	// clipPos/w needs to be done in pixel shader, not here
-	out.Write("	o.clipPos = o.pos;\n");
 
-	// TODO: Per Vertex Lighting?
+  // TODO: Per Vertex Lighting?
 
-	//write the true depth value, if the game uses depth textures pixel shaders will override with the correct values
-	//if not early z culling will improve speed
-	if (g_ActiveConfig.backend_info.bSupportsClipControl)
-	{
-		out.Write("	o.pos.z = -o.pos.z;\n");
-	}
-	else // OGL
-	{
-		// this results in a scale from -1..0 to -1..1 after perspective
-		// divide
-		out.Write("	o.pos.z = o.pos.z * -2.0 - o.pos.w;\n");
+  // clipPos/w needs to be done in pixel shader, not here
+  out.Write("o.clipPos = o.pos;\n");
 
-		// the next steps of the OGL pipeline are:
-		// (x_c,y_c,z_c,w_c) = o.pos  //switch to OGL spec terminology
-		// clipping to -w_c <= (x_c,y_c,z_c) <= w_c
-		// (x_d,y_d,z_d) = (x_c,y_c,z_c)/w_c//perspective divide
-		// z_w = (f-n)/2*z_d + (n+f)/2
-		// z_w now contains the value to go to the 0..1 depth buffer
+  // If we can disable the incorrect depth clipping planes using depth clamping, then we can do
+  // our own depth clipping and calculate the depth range before the perspective divide if
+  // necessary.
+  if (g_ActiveConfig.backend_info.bSupportsDepthClamp)
+  {
+    // Since we're adjusting z for the depth range before the perspective divide, we have to do our
+    // own clipping. We want to clip so that -w <= z <= 0, which matches the console -1..0 range.
+    // We adjust our depth value for clipping purposes to match the perspective projection in the
+    // software backend, which is a hack to fix Sonic Adventure and Unleashed games.
+    out.Write("float clipDepth = o.pos.z * (1.0 - 1e-7);\n");
+    out.Write("o.clipDist0 = clipDepth + o.pos.w;\n");  // Near: z < -w
+    out.Write("o.clipDist1 = -clipDepth;\n");           // Far: z > 0
+  }
 
-		//trying to get the correct semantic while not using glDepthRange
-		//seems to get rather complicated
-	}
+  // Write the true depth value. If the game uses depth textures, then the pixel shader will
+  // override it with the correct values if not then early z culling will improve speed.
+  // There are two different ways to do this, when the depth range is oversized, we process
+  // the depth range in the vertex shader, if not we let the host driver handle it.
+  //
+  // Adjust z for the depth range. We're using an equation which incorperates a depth inversion,
+  // so we can map the console -1..0 range to the 0..1 range used in the depth buffer.
+  // We have to handle the depth range in the vertex shader instead of after the perspective
+  // divide, because some games will use a depth range larger than what is allowed by the
+  // graphics API. These large depth ranges will still be clipped to the 0..1 range, so these
+  // games effectively add a depth bias to the values written to the depth buffer.
+  out.Write("o.pos.z = o.pos.w * " I_PIXELCENTERCORRECTION ".w - "
+            "o.pos.z * " I_PIXELCENTERCORRECTION ".z;\n");
 
-	// The console GPU places the pixel center at 7/12 in screen space unless
-	// antialiasing is enabled, while D3D and OpenGL place it at 0.5. This results
-	// in some primitives being placed one pixel too far to the bottom-right,
-	// which in turn can be critical if it happens for clear quads.
-	// Hence, we compensate for this pixel center difference so that primitives
-	// get rasterized correctly.
-	out.Write("	o.pos.xy = o.pos.xy - o.pos.w * " I_PIXELCENTERCORRECTION".xy;\n");
+  if (!g_ActiveConfig.backend_info.bSupportsClipControl)
+  {
+    // If the graphics API doesn't support a depth range of 0..1, then we need to map z to
+    // the -1..1 range. Unfortunately we have to use a substraction, which is a lossy floating-point
+    // operation that can introduce a round-trip error.
+    out.Write("o.pos.z = o.pos.z * 2.0 - o.pos.w;\n");
+  }
 
-	if (ApiType == APIType::OpenGL || ApiType == APIType::Vulkan)
-	{
-		// TODO: No Geometery shader fallback.
-		AssignVSOutputMembers(out, "vs", "o", numTexgen, false);
+  // Correct for negative viewports by mirroring all vertices. We need to negate the height here,
+  // since the viewport height is already negated by the render backend.
+  out.Write("o.pos.xy *= sign(" I_PIXELCENTERCORRECTION ".xy * float2(1.0, -1.0));\n");
 
-		out.Write("	gl_Position = o.pos;\n");
-	}
-	else // D3D
-	{
-		out.Write("	return o;\n");
-	}
-	out.Write("}\n");
+  // The console GPU places the pixel center at 7/12 in screen space unless
+  // antialiasing is enabled, while D3D and OpenGL place it at 0.5. This results
+  // in some primitives being placed one pixel too far to the bottom-right,
+  // which in turn can be critical if it happens for clear quads.
+  // Hence, we compensate for this pixel center difference so that primitives
+  // get rasterized correctly.
+  out.Write("o.pos.xy = o.pos.xy - o.pos.w * " I_PIXELCENTERCORRECTION ".xy;\n");
+
+  if (ApiType == APIType::OpenGL || ApiType == APIType::Vulkan)
+  {
+    if (g_ActiveConfig.backend_info.bSupportsGeometryShaders || ApiType == APIType::Vulkan)
+    {
+      AssignVSOutputMembers(out, "vs", "o", numTexgen, false);
+    }
+    else
+    {
+      // TODO: Pass interface blocks between shader stages even if geometry shaders
+      // are not supported, however that will require at least OpenGL 3.2 support.
+      for (u32 i = 0; i < numTexgen; ++i)
+        out.Write("uv%d.xyz = o.tex[%d];\n", i, i);
+      out.Write("clipPos = o.clipPos;\n");
+      out.Write("colors_0 = o.colors_0;\n");
+      out.Write("colors_1 = o.colors_1;\n");
+    }
+
+    if (g_ActiveConfig.backend_info.bSupportsDepthClamp)
+    {
+      out.Write("gl_ClipDistance[0] = o.clipDist0;\n");
+      out.Write("gl_ClipDistance[1] = o.clipDist1;\n");
+    }
+
+    // Vulkan NDC space has Y pointing down (right-handed NDC space).
+    if (ApiType == APIType::Vulkan)
+      out.Write("gl_Position = float4(o.pos.x, -o.pos.y, o.pos.z, o.pos.w);\n");
+    else
+      out.Write("gl_Position = o.pos;\n");
+  }
+  else  // D3D
+  {
+    out.Write("return o;\n");
+  }
+  out.Write("}\n");
 
 	return out;
 }
