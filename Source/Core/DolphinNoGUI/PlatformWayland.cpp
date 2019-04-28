ba81878d385d83d1a3ae4972a8c0b3f861213b5a
Stenzek <stenzek@gmail.com>
2019-04-28 23:04:09 +1000

    DolphinNoGUI: Use xdg-shell in Wayland platform

--- Source/Core/DolphinNoGUI/PlatformWayland.cpp
+++ Source/Core/DolphinNoGUI/PlatformWayland.cpp
@@ -16,6 +16,7 @@
 #include <cstring>
 
 #include <wayland-client-protocol.h>
+#include "xdg-shell-client-protocol.h"
 
 #include "UICommon/X11Utils.h"
 #include "VideoCommon/RenderBase.h"
@@ -36,36 +37,40 @@ public:
 private:
   void ProcessEvents();
 
-  static void ShellSurfacePing(void* data, wl_shell_surface* surface, uint32_t serial);
-  static void ShellSurfaceConfigure(void* data, wl_shell_surface* surface, uint32_t edges,
-                                    int32_t width, int32_t height);
-  static void ShellSurfacePopupDone(void* data, wl_shell_surface* surface);
   static void GlobalRegistryHandler(void* data, wl_registry* registry, uint32_t id,
                                     const char* interface, uint32_t version);
   static void GlobalRegistryRemover(void* data, wl_registry* registry, uint32_t id);
+  static void XDGWMBasePing(void* data, struct xdg_wm_base* xdg_wm_base, uint32_t serial);
+  static void XDGSurfaceConfigure(void* data, struct xdg_surface* xdg_surface, uint32_t serial);
+  static void TopLevelConfigure(void* data, struct xdg_toplevel* xdg_toplevel, int32_t width,
+                                int32_t height, struct wl_array* states);
+  static void TopLevelClose(void* data, struct xdg_toplevel* xdg_toplevel);
 
   wl_display* m_display = nullptr;
   wl_registry* m_registry = nullptr;
   wl_compositor* m_compositor = nullptr;
+  xdg_wm_base* m_xdg_wm_base = nullptr;
   wl_surface* m_surface = nullptr;
   wl_region* m_region = nullptr;
-  wl_shell* m_shell = nullptr;
-  wl_shell_surface* m_shell_surface = nullptr;
+  xdg_surface* m_xdg_surface = nullptr;
+  xdg_toplevel* m_xdg_toplevel = nullptr;
 
-  u32 m_surface_width = 0;
-  u32 m_surface_height = 0;
+  int m_surface_width = 0;
+  int m_surface_height = 0;
 };
 
 PlatformWayland::~PlatformWayland()
 {
-  if (m_shell_surface)
-    wl_shell_surface_destroy(m_shell_surface);
+  if (m_xdg_toplevel)
+    xdg_toplevel_destroy(m_xdg_toplevel);
+  if (m_xdg_surface)
+    xdg_surface_destroy(m_xdg_surface);
   if (m_surface)
     wl_surface_destroy(m_surface);
   if (m_region)
     wl_region_destroy(m_region);
-  if (m_shell)
-    wl_shell_destroy(m_shell);
+  if (m_xdg_wm_base)
+    xdg_wm_base_destroy(m_xdg_wm_base);
   if (m_compositor)
     wl_compositor_destroy(m_compositor);
   if (m_registry)
@@ -74,41 +79,55 @@ PlatformWayland::~PlatformWayland()
     wl_display_disconnect(m_display);
 }
 
-void PlatformWayland::ShellSurfacePing(void* data, wl_shell_surface* surface, uint32_t serial)
-{
-  wl_shell_surface_pong(surface, serial);
-}
-
-void PlatformWayland::ShellSurfaceConfigure(void* data, wl_shell_surface* surface, uint32_t edges,
-                                            int32_t width, int32_t height)
-{
-  PlatformWayland* platform = static_cast<PlatformWayland*>(data);
-  platform->m_surface_width = static_cast<u32>(width);
-  platform->m_surface_height = static_cast<u32>(height);
-}
-
-void PlatformWayland::ShellSurfacePopupDone(void* data, wl_shell_surface* surface)
-{
-}
-
 void PlatformWayland::GlobalRegistryHandler(void* data, wl_registry* registry, uint32_t id,
                                             const char* interface, uint32_t version)
 {
   PlatformWayland* platform = static_cast<PlatformWayland*>(data);
-  if (std::strcmp(interface, "wl_compositor") == 0)
+  if (std::strcmp(interface, wl_compositor_interface.name) == 0)
   {
     platform->m_compositor = static_cast<wl_compositor*>(
         wl_registry_bind(platform->m_registry, id, &wl_compositor_interface, 1));
   }
-  else if (std::strcmp(interface, "wl_shell") == 0)
+  else if (std::strcmp(interface, xdg_wm_base_interface.name) == 0)
   {
-    platform->m_shell =
-        static_cast<wl_shell*>(wl_registry_bind(platform->m_registry, id, &wl_shell_interface, 1));
+    platform->m_xdg_wm_base = static_cast<xdg_wm_base*>(
+        wl_registry_bind(platform->m_registry, id, &xdg_wm_base_interface, 1));
   }
 }
 
 void PlatformWayland::GlobalRegistryRemover(void* data, wl_registry* registry, uint32_t id)
 {
+}
+
+void PlatformWayland::XDGWMBasePing(void* data, struct xdg_wm_base* xdg_wm_base, uint32_t serial)
+{
+  xdg_wm_base_pong(xdg_wm_base, serial);
+}
+
+void PlatformWayland::XDGSurfaceConfigure(void* data, struct xdg_surface* xdg_surface,
+                                          uint32_t serial)
+{
+  xdg_surface_ack_configure(xdg_surface, serial);
+}
+
+void PlatformWayland::TopLevelConfigure(void* data, struct xdg_toplevel* xdg_toplevel,
+                                        int32_t width, int32_t height, struct wl_array* states)
+{
+  // If this is zero, it's asking us to set the size.
+  if (width == 0 || height == 0)
+    return;
+
+  PlatformWayland* platform = static_cast<PlatformWayland*>(data);
+  platform->m_surface_width = width;
+  platform->m_surface_height = height;
+  if (g_renderer)
+    g_renderer->ResizeSurface(width, height);
+}
+
+void PlatformWayland::TopLevelClose(void* data, struct xdg_toplevel* xdg_toplevel)
+{
+  PlatformWayland* platform = static_cast<PlatformWayland*>(data);
+  platform->Stop();
 }
 
 bool PlatformWayland::Init()
@@ -130,38 +149,50 @@ bool PlatformWayland::Init()
   wl_display_roundtrip(m_display);
 
   // We need a shell/compositor, or at least one we understand.
-  if (!m_compositor || !m_display)
+  if (!m_compositor || !m_display || !m_xdg_wm_base)
   {
     std::fprintf(stderr, "Missing Wayland shell/compositor\n");
     return false;
   }
 
   // Create the compositor and shell surface.
-  m_surface_width = Config::Get(Config::MAIN_RENDER_WINDOW_WIDTH);
-  m_surface_height = Config::Get(Config::MAIN_RENDER_WINDOW_HEIGHT);
   if (!(m_surface = wl_compositor_create_surface(m_compositor)) ||
-      !(m_shell_surface = wl_shell_get_shell_surface(m_shell, m_surface)))
+      !(m_xdg_surface = xdg_wm_base_get_xdg_surface(m_xdg_wm_base, m_surface)) ||
+      !(m_xdg_toplevel = xdg_surface_get_toplevel(m_xdg_surface)))
   {
     std::fprintf(stderr, "Failed to create compositor/shell surfaces\n");
     return false;
   }
 
-  static const wl_shell_surface_listener shell_surface_listener = {
-      ShellSurfacePing, ShellSurfaceConfigure, ShellSurfacePopupDone};
-  wl_shell_surface_add_listener(m_shell_surface, &shell_surface_listener, this);
-  wl_shell_surface_set_toplevel(m_shell_surface);
+  static const xdg_wm_base_listener xdg_wm_base_listener = {XDGWMBasePing};
+  xdg_wm_base_add_listener(m_xdg_wm_base, &xdg_wm_base_listener, this);
+
+  static const xdg_surface_listener shell_surface_listener = {XDGSurfaceConfigure};
+  xdg_surface_add_listener(m_xdg_surface, &shell_surface_listener, this);
+
+  static const xdg_toplevel_listener toplevel_listener = {TopLevelConfigure, TopLevelClose};
+  xdg_toplevel_add_listener(m_xdg_toplevel, &toplevel_listener, this);
 
   // Create region in the surface to draw into.
+  m_surface_width = Config::Get(Config::MAIN_RENDER_WINDOW_WIDTH);
+  m_surface_height = Config::Get(Config::MAIN_RENDER_WINDOW_HEIGHT);
   m_region = wl_compositor_create_region(m_compositor);
-  wl_region_add(m_region, Config::Get(Config::MAIN_RENDER_WINDOW_XPOS),
-                Config::Get(Config::MAIN_RENDER_WINDOW_YPOS), m_surface_width, m_surface_height);
+  wl_region_add(m_region, 0, 0, m_surface_width, m_surface_height);
   wl_surface_set_opaque_region(m_surface, m_region);
+  wl_surface_commit(m_surface);
+
+  // This doesn't seem to have any effect on kwin...
+  xdg_surface_set_window_geometry(m_xdg_surface, Config::Get(Config::MAIN_RENDER_WINDOW_XPOS),
+                                  Config::Get(Config::MAIN_RENDER_WINDOW_YPOS),
+                                  Config::Get(Config::MAIN_RENDER_WINDOW_WIDTH),
+                                  Config::Get(Config::MAIN_RENDER_WINDOW_HEIGHT));
+
   return true;
 }
 
 void PlatformWayland::SetTitle(const std::string& string)
 {
-  wl_shell_surface_set_title(m_shell_surface, string.c_str());
+  xdg_toplevel_set_title(m_xdg_toplevel, string.c_str());
 }
 
 void PlatformWayland::MainLoop()
